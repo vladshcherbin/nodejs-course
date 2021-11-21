1f87db033342fc0d5ea35d7ade6685516db3f249
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-11-21 20:37:23 +0300

    Extract config value parser

---
+++ ciphering-cli/src/config-parser.js
@@ -0,0 +1,26 @@
+import {
+  AtbashEncodeStream,
+  CaesarDecodeStream,
+  CaesarEncodeStream,
+  Rot8DecodeStream,
+  Rot8EncodeStream
+} from './ciphers'
+
+export default function parseConfig(config) {
+  return config.split('-').map((value) => {
+    switch (value) {
+      case 'A':
+        return new AtbashEncodeStream()
+      case 'C0':
+        return new CaesarDecodeStream()
+      case 'C1':
+        return new CaesarEncodeStream()
+      case 'R0':
+        return new Rot8DecodeStream()
+      case 'R1':
+        return new Rot8EncodeStream()
+      default:
+        throw new Error('Unknown "config" pattern')
+    }
+  })
+}
--- ciphering-cli/src/index.js
+++ ciphering-cli/src/index.js
@@ -4,39 +4,17 @@ import ReadStream from './ReadStream'
 import UserError from './UserError'
 import WriteStream from './WriteStream'
 import parseArguments from './arguments-parser'
-import {
-  AtbashEncodeStream,
-  CaesarDecodeStream,
-  CaesarEncodeStream,
-  Rot8DecodeStream,
-  Rot8EncodeStream
-} from './ciphers'
+import parseConfig from './config-parser'
 import validateOptions from './options-validation'
 
 try {
   const options = parseArguments(process.argv.slice(2))
   const { config, input, output } = await validateOptions(options)
-
-  const patterns = config.split('-').map((value) => {
-    switch (value) {
-      case 'A':
-        return new AtbashEncodeStream()
-      case 'C0':
-        return new CaesarDecodeStream()
-      case 'C1':
-        return new CaesarEncodeStream()
-      case 'R0':
-        return new Rot8DecodeStream()
-      case 'R1':
-        return new Rot8EncodeStream()
-      default:
-        throw new Error('Unknown "config" pattern')
-    }
-  })
+  const ciphers = parseConfig(config)
 
   await pipeline(
     input ? new ReadStream(input) : process.stdin,
-    ...patterns,
+    ...ciphers,
     output ? new WriteStream(output) : process.stdout
   )
 } catch (error) {
