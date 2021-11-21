b65f74d0b00c55b148fce828654728b1f4a5fcdf
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-11-21 20:28:32 +0300

    Update parsed arguments position

--- ciphering-cli/src/arguments-parser.js
+++ ciphering-cli/src/arguments-parser.js
@@ -1,28 +1,29 @@
 import UserError from './UserError'
 
-const parsedArguments = {}
-
 function parseArgumentValue(argumentIndex, args) {
   const argumentValue = args[argumentIndex + 1]
 
   return (argumentValue && !argumentValue.startsWith('-')) ? argumentValue : true
 }
 
-function parseOption(option, flags, [argument, index], args) {
+function parseOption(option, flags, [argument, index], args, parsedArguments) {
   if (flags.includes(argument)) {
     if (parsedArguments[option]) {
       throw new UserError(`"${option}" option is duplicated`)
     }
 
+    // eslint-disable-next-line no-param-reassign
     parsedArguments[option] = parseArgumentValue(index, args)
   }
 }
 
 export default function parseArguments(args) {
+  const parsedArguments = {}
+
   args.forEach((...argument) => {
-    parseOption('config', ['-c', '--config'], argument, args)
-    parseOption('input', ['-i', '--input'], argument, args)
-    parseOption('output', ['-o', '--output'], argument, args)
+    parseOption('config', ['-c', '--config'], argument, args, parsedArguments)
+    parseOption('input', ['-i', '--input'], argument, args, parsedArguments)
+    parseOption('output', ['-o', '--output'], argument, args, parsedArguments)
   })
 
   return parsedArguments
